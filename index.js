ed57a150b3bc428278001d71a4eabc4b7b0657bd
Fahim-H <fahimlutful@gmail.com>
2022-10-24 23:47:36 +0600

    added json data

--- index.js
+++ index.js
@@ -2,8 +2,13 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 4000;
 
+const courseData = require("./data/courses.json");
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+app.get("/course-data", (req, res) => {
+  res.send(courseData);
 });
 
 app.listen(port, () => {
